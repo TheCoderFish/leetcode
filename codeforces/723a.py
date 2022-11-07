85dc9ea305d4d70cb893c0c66d51dffd8176a0af
Nurali Khoja <nurali@tradexa.co.in>
2022-11-07 09:17:16 +0530

    07-11

---
+++ codeforces/723a.py
@@ -0,0 +1,2 @@
+loc = sorted(map(int, input().split(' ')))
+print((loc[1] - loc[0]) + (loc[2] - loc[1]))
--- codeforces/732a.py
+++ codeforces/732a.py
@@ -5,7 +5,7 @@ k, r = map(int, input().split())
 
 t = k
 n = 0
-while not (t % 10 == 0 or (t - r) % 10 == 0):
+while not (t % 10 == 0 or t % 10 == r):
     n += 1
     t += k
 
