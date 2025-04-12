c43f791e75d39a79ed6cd786f8cf4c69daa844d8
Nurali Khoja <nuralikhoja@gmail.com>
2025-04-12 17:08:49 +0530

    Update topk.py

--- topk.py
+++ topk.py
@@ -5,10 +5,25 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        return heapq.nlargest(k, count.keys(), key=lambda n: count[n])
+        # get the counts
+        # prepare the buckets by freq
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
 
-nums = [1, 1, 1, 3, 3, 3, 2, 2, 2,2]
+nums = [1, 1, 1, 3, 3, 3, 2, 2, 2, 2]
 
 print(Solution().topKFrequent(nums, k=2))
